51d9eecbfb766aafd1fdd6fc9d309a177b439457
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2025-11-04 14:33:32 +0100

    Fix migrations

--- app/Database/Migrations/2025-05-28-100247_AddFieldTypesToFilledFiles.php
+++ app/Database/Migrations/2025-05-28-100247_AddFieldTypesToFilledFiles.php
@@ -8,7 +8,9 @@ class AddFieldTypesToFilledFiles extends Migration
 {
     public function up()
     {
-        if (!$this->columnExists('filledFiles', 'fieldTypes')) {
+        $db = \Config\Database::connect();
+
+        if (!$db->fieldExists('fieldTypes', 'filledFiles')) {
             $this->forge->addColumn('filledFiles', [
                 'fieldTypes' => [
                     'type' => 'TEXT',
@@ -21,33 +23,10 @@ class AddFieldTypesToFilledFiles extends Migration
 
     public function down()
     {
-        if ($this->columnExists('filledFiles', 'fieldTypes')) {
+        $db = \Config\Database::connect();
+
+        if ($db->fieldExists('fieldTypes', 'filledFiles')) {
             $this->forge->dropColumn('filledFiles', 'fieldTypes');
-        }
-    }
-
-    private function columnExists($table, $column)
-    {
-        $dbDriver = $this->db->getPlatform();
-
-        if (stripos($dbDriver, 'mysql') !== false) {
-            // MySQL approach
-            $query = $this->db->query("SHOW COLUMNS FROM `{$table}` LIKE '{$column}'");
-            return $query->getNumRows() > 0;
-        } else {
-            // Generic approach for other databases (including SQLite)
-            try {
-                $query = $this->db->query("SELECT * FROM pragma_table_info('{$table}') WHERE name = '{$column}'");
-                return $query->getNumRows() > 0;
-            } catch (\Exception $e) {
-                // Fallback approach
-                try {
-                    $this->db->query("SELECT {$column} FROM {$table} LIMIT 1");
-                    return true;
-                } catch (\Exception $e) {
-                    return false;
-                }
-            }
         }
     }
 }
